c9374351c50dc467d82ed2ec1c363a203f8a0186
SP4MM3R-H4R5X <95898659+SP4MM3R-H4R5X@users.noreply.github.com>
2022-01-29 20:46:36 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -89,7 +89,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: Aditya Halder (@AdityaHalder)",
+        f"Powered By: HARSH (@HARSH1813)",
         (255, 255, 255),
         font=font,
     )
